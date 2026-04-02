ed1e76f16997db0f3907e8b1ec366247c7ef40cd
1abykeev <aktan.abykeev@gmail.com>
2026-04-02 20:03:33 +0300

    schemas added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Third party
+    "django_extensions",
     "rest_framework",
     "rest_framework_simplejwt",
     "corsheaders",
